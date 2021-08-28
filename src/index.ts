ce9e4c1209e059258b48848bd20d7202e271d72b
Daniel Waltz <danielbwaltz@gmail.com>
2021-08-28 02:53:21 -0400

    feat: Check if file matches a codegen document

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,38 @@
-import { Plugin } from 'vite';
+import { Plugin, ResolvedConfig } from 'vite';
 import { CodegenContext, generate, loadContext } from '@graphql-codegen/cli';
+import minimatch from 'minimatch';
 
 export default function VitePluginGraphQLCodegen(): Plugin {
+  let viteConfig: ResolvedConfig;
   let codegenContext: CodegenContext;
 
   return {
     name: 'graphql-codegen',
     async config(config) {
-      // Load codegen config from user defined project root
       codegenContext = await loadContext(config.root);
 
       // Vite handles file watching
       codegenContext.updateConfig({ watch: false });
     },
+    configResolved(resolvedConfig) {
+      viteConfig = resolvedConfig;
+    },
     async buildStart() {
       await generate(codegenContext);
     },
     configureServer(server) {
-      const listener = (path: string) => {
-        const codegenConfig = codegenContext.getConfig();
+      const listener = (path = '') => {
+        const { generates, documents } = codegenContext.getConfig();
 
-        // Get list of generated files
-        const generated = Object.keys(codegenConfig.generates);
-
-        // Check if saved file is a generated file
+        // Check if file is a generated file
+        const generated = Object.keys(generates);
         const isGenerated = generated.some(file => path.includes(file));
-
         if (isGenerated) return;
+
+        // Check if file is an operation document
+        const [, fileName = ''] = path.split(`${viteConfig.root}/`);
+        const isDocument = minimatch(fileName, documents);
+        if (!isDocument) return;
 
         generate(codegenContext);
       };
