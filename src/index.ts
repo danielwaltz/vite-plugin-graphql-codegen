82a017f8c4fc13aee9a152841bdc4e781fdae8cd
Daniel Waltz <danielbwaltz@gmail.com>
2021-08-28 00:56:43 -0400

    feat: Add simple watcher that runs codegen generate

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Plugin } from 'vite';
+import { generate, loadContext } from '@graphql-codegen/cli';
 
 export default function VitePluginGraphQLCodegen(): Plugin {
   return {
     name: 'graphql-codegen',
+    configureServer(server) {
+      server.watcher.on('change', async () => {
+        const context = await loadContext();
+        context.updateConfig({ watch: false });
+        generate(context, true);
+      });
+    },
   };
 }
