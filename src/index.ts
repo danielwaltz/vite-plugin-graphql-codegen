a67957bac2356c0ae791bb968b9f96623e6fbf9d
Daniel Waltz <danielbwaltz@gmail.com>
2021-08-28 02:24:05 -0400

    feat: Generate on build and skip generated files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,38 @@
 import { Plugin } from 'vite';
-import { generate, loadContext } from '@graphql-codegen/cli';
+import { CodegenContext, generate, loadContext } from '@graphql-codegen/cli';
 
 export default function VitePluginGraphQLCodegen(): Plugin {
+  let codegenContext: CodegenContext;
+
   return {
     name: 'graphql-codegen',
+    async config(config) {
+      // Load codegen config from user defined project root
+      codegenContext = await loadContext(config.root);
+
+      // Vite handles file watching
+      codegenContext.updateConfig({ watch: false });
+    },
+    async buildStart() {
+      await generate(codegenContext);
+    },
     configureServer(server) {
-      server.watcher.on('change', async () => {
-        const context = await loadContext();
-        context.updateConfig({ watch: false });
-        generate(context, true);
-      });
+      const listener = (path: string) => {
+        const codegenConfig = codegenContext.getConfig();
+
+        // Get list of generated files
+        const generated = Object.keys(codegenConfig.generates);
+
+        // Check if saved file is a generated file
+        const isGenerated = generated.some(file => path.includes(file));
+
+        if (isGenerated) return;
+
+        generate(codegenContext);
+      };
+
+      server.watcher.on('add', listener);
+      server.watcher.on('change', listener);
     },
   };
 }
