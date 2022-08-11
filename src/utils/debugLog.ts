c33bba7e351ecb1fc09f015e949d539b3c804f26
Daniel Waltz <danielbwaltz@gmail.com>
2022-08-11 17:10:18 -0400

    feat: update debug log output to match vite 3

--- src/utils/debugLog.ts
+++ src/utils/debugLog.ts
@@ -1,4 +1,10 @@
+const RESET = '\x1b[0m';
+const BRIGHT = '\x1b[1m';
+const DIM = '\x1b[2m';
+const FG_CYAN = '\x1b[36m';
+
 export function debugLog(...args: unknown[]) {
-  const LOG_PREFIX = '\x1b[36m[vite-plugin-graphql-codegen]\x1b[0m' as const;
-  console.log(LOG_PREFIX, ...args);
+  const LOG_PREFIX =
+    `  ${FG_CYAN}${BRIGHT}VITE PLUGIN GRAPHQL CODEGEN${RESET} ` as const;
+  console.log(LOG_PREFIX, DIM, ...args, RESET);
 }
