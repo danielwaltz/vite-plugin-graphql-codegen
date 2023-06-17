ff9a2c9c5e04a226019e6c93f846a7db39f5d88b
Daniel Waltz <danielbwaltz@gmail.com>
2023-06-17 07:48:32 -0400

    chore: refresh config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
-    es2021: true,
     node: true,
+    es2022: true,
   },
   extends: [
     'eslint:recommended',
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     testTimeout: 10000,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
 });
