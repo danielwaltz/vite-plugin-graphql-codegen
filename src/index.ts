529cd52d7307cf83fbb857ff5b3195f4a3e35de6
Daniel Waltz <danielbwaltz@gmail.com>
2023-08-14 16:54:08 -0400

    style: use type imports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import { Plugin } from 'vite';
-import { CodegenContext, generate, loadContext } from '@graphql-codegen/cli';
-import { Types } from '@graphql-codegen/plugin-helpers';
+import { type Plugin } from 'vite';
+import {
+  type CodegenConfig,
+  CodegenContext,
+  generate,
+  loadContext,
+} from '@graphql-codegen/cli';
 import {
   isCodegenConfig,
   isGraphQLDocument,
   isGraphQLSchema,
 } from '@/utils/fileMatchers';
-import { ViteMode, isServeMode, isBuildMode } from '@/utils/viteModes';
+import { isBuildMode, isServeMode, type ViteMode } from '@/utils/viteModes';
 import { debugLog } from '@/utils/debugLog';
-
-type CodegenConfig = Types.Config;
 
 export interface Options {
   /**
