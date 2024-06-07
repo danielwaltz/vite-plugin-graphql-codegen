baafe70c5774be846767806efb4d743c86fa3683
yurk <yurkins@gmail.com>
2024-06-07 16:30:38 +0300

    chore: rewrite file watcher

--- src/utils/fileMatchers.ts
+++ src/utils/fileMatchers.ts
@@ -1,34 +1,15 @@
 import { normalizePath } from 'vite';
-import type { CodegenContext } from '@graphql-codegen/cli';
-import { getDocumentPaths, getSchemaPaths } from '../utils/configPaths';
 
-export type FileMatcher = (
-  filePath: string,
-  context: CodegenContext,
-) => Promise<boolean>;
+export const isCodegenConfig = (filePath: string, configPath?: string) => {
+  if (!configPath) return false;
 
-export const isCodegenConfig: FileMatcher = async (filePath, context) => {
-  if (!context.filepath) return false;
-
-  return normalizePath(filePath) === normalizePath(context.filepath);
+  return normalizePath(filePath) === normalizePath(configPath);
 };
 
-export const isGraphQLDocument: FileMatcher = async (filePath, context) => {
-  const documentPaths = await getDocumentPaths(context);
-
-  if (!documentPaths.length) return false;
+export const isFileMatched = (filePath: string, lookupPaths: string[]) => {
+  if (!lookupPaths.length) return false;
 
   const normalizedFilePath = normalizePath(filePath);
 
-  return documentPaths.some((path) => normalizedFilePath.includes(path));
-};
-
-export const isGraphQLSchema: FileMatcher = async (filePath, context) => {
-  const schemaPaths = await getSchemaPaths(context);
-
-  if (!schemaPaths.length) return false;
-
-  const normalizedFilePath = normalizePath(filePath);
-
-  return schemaPaths.some((path) => normalizedFilePath.includes(path));
+  return lookupPaths.includes(normalizedFilePath);
 };
